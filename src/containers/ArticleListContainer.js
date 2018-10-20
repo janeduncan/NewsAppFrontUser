8690115a220d8e8142e6a534b4e25034a45b9d0a
Ricky <rickycorrigan@icloud.com>
2018-10-20 11:52:47 +0100

    adds routes

--- src/containers/ArticleListContainer.js
+++ src/containers/ArticleListContainer.js
@@ -1,9 +1,16 @@
 import React, {Component} from 'react';
 
 const ArticleListContainer = (props) => {
+
+  console.log(props.articles);
   return (
-    <div className="articles">
-      article
+    <div className="articles-container">
+      <h3 className="heading">heading</h3>
+        <div>
+          <p>hello article</p>
+        </div>
     </div>
   )
 }
+
+export default ArticleListContainer;
