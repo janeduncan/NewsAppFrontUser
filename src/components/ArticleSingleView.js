90b1bdc19294c50084aec5060547ac27d5468d55
Ricky <rickycorrigan@icloud.com>
2018-10-22 15:28:26 +0100

    adds single view component

---
+++ src/components/ArticleSingleView.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import moment from 'moment';
+
+const ArticleSingleView = (props) => {
+
+  const date = new Date(props.article.date);
+  const formattedDate = moment(date).format("DD/MM/YYYY HH:mm");
+
+  return (
+    <div>
+      <p>{formattedDate}</p>
+      <p>{props.article.journalist.name}</p>
+      <p>{props.article.image}</p>
+      <option>{props.article.category}</option>
+      <p>{props.article.title}</p>
+      <p>{props.article.text}</p>
+    </div>
+  )
+}
+
+export default ArticleSingleView;
